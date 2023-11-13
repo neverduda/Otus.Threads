62e8caa342a6241880200cc6bdf88da6cf9b9ac3
Сергей Дудкин <neverduda@yandex.ru>
2023-11-14 00:49:36 +0300

    Tasks

--- Otus.Threads/Program.cs
+++ Otus.Threads/Program.cs
@@ -4,7 +4,7 @@ namespace Otus.Threads
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Console.WriteLine("Введите путь до каталога с файлами:");
             // var folderPath = Console.ReadLine();
@@ -14,7 +14,7 @@ namespace Otus.Threads
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            var spaces = CountSpacesInFolderAsync(folderPath);
+            var spaces = await CountSpacesInFolderAsync(folderPath);
 
             stopwatch.Stop();
             Console.WriteLine($"Общее количество пробелов в файлах: {spaces}");
@@ -22,19 +22,29 @@ namespace Otus.Threads
             Console.ReadKey();
         }
 
-        static int CountSpacesInFolderAsync(string folderPath)
+        static async Task<int> CountSpacesInFolderAsync(string folderPath)
         {
             string[] filePathes = Directory.GetFiles(folderPath);
             int totalSpaces = 0;
-            Parallel.ForEach(filePathes, (filePath) =>
+
+            Task<int>[] tasks = new Task<int>[filePathes.Length];
+
+            for (int i = 0; i < filePathes.Length; i++)
             {
-                var spaces = CountSpaces(filePath);
-                Interlocked.Add(ref totalSpaces, spaces);
-            });
+                tasks[i] = CountSpacesAsync(filePathes[i]);
+            }
+
+            await Task.WhenAll(tasks);
+
+            foreach (var task in tasks)
+            {
+                totalSpaces += task.Result;
+            }
+
             return totalSpaces;
         }
 
-        static int CountSpaces(string filePath)
+        static async Task<int> CountSpacesAsync(string filePath)
         {
             string text = File.ReadAllText(filePath);
             int spaces = text.Count(c => c == ' ');
